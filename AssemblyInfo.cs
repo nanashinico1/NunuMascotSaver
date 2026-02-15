7e4eb30594af3604f0d66fc91c16635c4a08c938
nanashinico1 <nanashinico1@gmail/com>
2026-02-15 17:41:48 +0900

    Add AssemblyInfo and update settings form

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("nanashinico1")]
+[assembly: AssemblyProduct("NunuMascotSaver")]
+[assembly: AssemblyTitle("Nunu Mascot Saver")]
+[assembly: AssemblyDescription("Nunu Mascot Saver for Windows")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
